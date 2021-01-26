c921e4bea0cf7e8bc66db97eaa82526c20ed4b60
CS Balotiya <74857569+csbalotiya@users.noreply.github.com>
2021-01-26 14:23:46 +0530

    Create geometryFormula.java

---
+++ Maths Formulas App/geometryFormula.java
@@ -0,0 +1,17 @@
+package com.example.mathsformulas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class geometryFormulas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_geometry_formulas);
+        Intent intent = getIntent();
+
+    }
+}
