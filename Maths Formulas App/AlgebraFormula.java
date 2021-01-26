e4612f7092714a7519ce3abc5ea9abdd39888705
CS Balotiya <74857569+csbalotiya@users.noreply.github.com>
2021-01-26 14:22:38 +0530

    Create AlgebraFormula.java

---
+++ Maths Formulas App/AlgebraFormula.java
@@ -0,0 +1,17 @@
+package com.example.mathsformulas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class AlgebraFormula extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_algebra_formula);
+
+        Intent intent = getIntent();
+    }
+}
